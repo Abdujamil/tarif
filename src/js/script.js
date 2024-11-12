ce09d34e4d5164c6a9e7618ff29faefb24643adf
Abdujamil <suhrobgafarov3887@gmail.com>
2024-11-12 18:36:10 +0500

    responsive done

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,30 @@
 // Начальное значение таймера
-let hours = 22;
-let minutes = 36;
+let hours = 0;
+let minutes = 0;
 let seconds = 10;
 
 // Функция для обновления отображения таймера
 function updateTimerDisplay() {
-  const timerElement = document.getElementById("timer");
-  const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-  timerElement.textContent = formattedTime;
+  const timerElements = document.querySelectorAll(".timer");  // Используем querySelectorAll для всех элементов с классом
+  timerElements.forEach(timerElement => {
+    const formattedTime = `${String(hours).padStart(2, "0")}:${String(
+      minutes
+    ).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+    timerElement.textContent = formattedTime;
+  });
 }
 
 // Функция обратного отсчета
 function countdown() {
   if (hours === 0 && minutes === 0 && seconds === 0) {
     clearInterval(timerInterval);
-    document.getElementById("selectButton").disabled = true; // Отключаем кнопку
+    
+    // Отключаем все кнопки с классом "selectButton"
+    const selectButtons = document.querySelectorAll(".disableButton"); // Используем querySelectorAll для всех кнопок с классом
+    selectButtons.forEach(selectButton => {
+      selectButton.disabled = true; // Добавляем атрибут disabled
+    });
+
     return;
   }
 
@@ -36,3 +46,29 @@ function countdown() {
 // Запускаем таймер при загрузке страницы
 updateTimerDisplay();
 const timerInterval = setInterval(countdown, 1000);
+
+// Клик по карточка
+let cardConsl = document.querySelectorAll(".cardConsl");
+let cardBazTarif = document.querySelectorAll(".cardBazTarif");
+let cardRass = document.querySelectorAll(".cardRass");
+
+// Добавляем обработчик клика для каждой карточки "Консультация"
+cardConsl.forEach((card) => {
+  card.onclick = () => {
+    console.log("Карточка: Консультация");
+  };
+});
+
+// Добавляем обработчик клика для каждой карточки "Базовый тариф"
+cardBazTarif.forEach((card) => {
+  card.onclick = () => {
+    console.log("Карточка: Базовый тариф");
+  };
+});
+
+// Добавляем обработчик клика для каждой карточки "Рассрочка"
+cardRass.forEach((card) => {
+  card.onclick = () => {
+    console.log("Карточка: Рассрочка");
+  };
+});
